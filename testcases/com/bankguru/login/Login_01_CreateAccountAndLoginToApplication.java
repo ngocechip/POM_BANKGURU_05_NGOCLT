c8d084b53236dbacfd5a923259a05544b3105edd
Le Thi Ngoc <ngocechip@gmail.com>
2018-08-15 23:46:44 +0700

    "Submit POM_Topic_01!"

---
+++ testcases/com/bankguru/login/Login_01_CreateAccountAndLoginToApplication.java
@@ -0,0 +1,60 @@
+package com.bankguru.login;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Login_01_CreateAccountAndLoginToApplication {
+	WebDriver driver;
+	String loginUrl, email, userId, password;
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		email = "automationfc" + randomEmail() + "@gmail.com";
+	}
+
+	@Test
+	public void TC_01_CreateAnAccount() {
+		driver.get("http://demo.guru99.com/v4/");
+		loginUrl = driver.getCurrentUrl();
+		driver.findElement(By.xpath("//a[text()='here']")).click();
+		driver.findElement(By.xpath("//input[@name='emailid']")).sendKeys(email);
+		driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
+		userId = driver.findElement(By.xpath("//td[text()='User ID :']//following-sibling::td")).getText();
+		password = driver.findElement(By.xpath("//td[text()='Password :']//following-sibling::td")).getText();
+
+	}
+
+	@Test
+	public void TC_02_LoginWithAboveInformation() {
+		driver.get(loginUrl);
+		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(userId);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+		driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//marquee[text()=\"Welcome To Manager's Page of Guru99 Bank\"]"))
+				.isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//td[text()='Manger Id : " + userId + "']")).isDisplayed());
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	public int randomEmail() {
+		Random random = new Random();
+		int number = random.nextInt(999999);
+		return number;
+	}
+}
---
+++ testcases/com/bankguru/login/Login_02_ValidatedLoginPage.java
@@ -0,0 +1,19 @@
+package com.bankguru.login;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Login_02_ValidatedLoginPage {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
